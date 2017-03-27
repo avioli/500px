f6cdb5eb7548fae87f032767898a10094d2ebebe
Evo Stamatov <aviolito@gmail.com>
2017-03-27 15:40:20 +1100

    add app.json to allow heroku deploys

---
+++ app.js
@@ -0,0 +1,14 @@
+{
+  "name": "500px",
+  "description": "A simple iOS 9 web app to capture photos and resize them to 500px",
+  "repository": "https://github.com/avioli/500px",
+  "keywords": [
+    "ios",
+    "ios9",
+    "camera",
+    "resize",
+    "photo",
+    "capture",
+    "web app"
+  ]
+}
